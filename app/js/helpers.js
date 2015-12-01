612647ba0c4a9250bec0f70e69bcde5b9d77c4de
Nate Martin <nmartin@agorafinancial.com>
2015-12-01 18:54:56 -0500

    added environment variables js file

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -1,5 +1,18 @@
 
+var environment = require('./.environment.js');
+
 module.exports.concat = function(str_a, str_b)
 {
     return str_a + str_b
+}
+
+module.exports.site_url = function(path)
+{
+    path = (typeof path == 'string') ? path.replace(/(^\/|\/$)/g, '') : '';
+    return 'http://' + environment.blog_url + '/' + path;
+}
+
+module.exports.asset_cdn = function()
+{
+    return environment.asset_cdn;
 }
