6b552b3d531f48e0355bec754d7d782b1b705001
Nate Martin <nmartin@agorafinancial.com>
2015-12-02 22:33:37 -0500

    added post Kulinarian.com Users Asked For A New Website

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -6,21 +6,37 @@ module.exports.concat = function(str_a, str_b)
     return str_a + str_b
 }
 
-module.exports.site_url = function(path, ssl)
+module.exports.siteUrl = function(path, ssl)
 {
     path = (typeof path == 'string') ? path.replace(/(^\/|\/$)/g, '') : '';
     protocol = ssl === true ? 'https://' : 'http://';
     return protocol + environment.blog_url + '/' + path;
 }
 
-module.exports.app_url = function(path, ssl)
+/**
+ * this is for pointing people to our product, instead of this blog
+ * @param  {[type]} path [description]
+ * @param  {[type]} ssl  [description]
+ * @return {[type]}      [description]
+ */
+module.exports.appUrl = function(path, ssl)
 {
     path = (typeof path == 'string') ? path.replace(/(^\/|\/$)/g, '') : '';
     protocol = ssl === true ? 'https://' : 'http://';
     return protocol + environment.app_url + '/' + path;
 }
 
-module.exports.asset_cdn = function()
+module.exports.assetCdn = function(path)
 {
-    return environment.asset_cdn;
+    path = (typeof path == 'string') ? '/' + path.replace(/(^\/|\/$)/g, '') : '';
+    return environment.asset_cdn + path;
+}
+
+/**
+ * remove the 'dist' from our urls
+ * @return {[type]} [description]
+ */
+module.exports.pageUrl = function(url)
+{
+    return url.replace(/dist\//, '');
 }
